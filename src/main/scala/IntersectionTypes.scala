5d3b7ef54d82aed96e04b083a87f239a4dff17fe
aamirfayaz <dropmeataamir@gmail.com>
2021-12-28 10:17:24 +0530

    inter types examples

--- src/main/scala/IntersectionTypes.scala
+++ src/main/scala/IntersectionTypes.scala
@@ -18,4 +18,46 @@
 	class SmartPhone extends Camera with Phone
 
 	useSmartDevice(new SmartPhone)
+
+	trait Camera2 {
+
+		def use() = println("camera use")
+	}
+
+	trait Phone2 {
+
+		def use() = println("phone use")
+	}
+
+	def useSmartDevice2(sp: Camera2 & Phone2) = {
+		sp.use()
+	}
+
+	class SmartPhone2 extends Camera2 with Phone2 {
+		override def use() = println("smart use")
+	}
+
+	useSmartDevice2(new SmartPhone2)
+
+	trait HostConfig
+	trait HostController {
+		def get: Option[HostConfig]
+	}
+	trait PortConfig
+	trait PortController {
+		def get: Option[PortConfig]
+	}
+
+	def getConfigs(controller: HostController & PortController): Option[HostConfig & PortConfig] = {
+		controller.get
+	}
+
+// class Config extends HostConfig CTE
+class Config extends HostConfig with PortConfig
+
+
+	class Controller extends HostController with PortController {
+		//override def get = None
+		override def get:Option[HostConfig & PortConfig] = Some(new Config)
+	}
 }
